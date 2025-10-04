422d0f1bf774102ad20eda8555e25b8f4595bd92
Geovana Bicalho <geovanambicalho@hotmail.com>
2025-10-04 11:22:45 -0300

    Mapeamento orçamento

---
+++ exercicios-individuais/projetos/orcamento/orcamento.js
@@ -0,0 +1,11 @@
+document.querySelector('.seu-nome') .textContent = "Geovana Moreira Bicalho";
+
+const inputPaginas = document.querySelector("#qtd-paginas");
+const inputDesconto = document.querySelector("#desconto");
+const checkboxDesing = document.querySelector("#inclui-design");
+const inputPrazo = document.querySelector("#prazo-entrega");
+const resumoSubTotal = document.querySelector("#resumo-subtotal");
+const resumoAdicional = document.querySelector("#resumo-adicional");
+const resumoUrgencia = document.querySelector("#resumo-urgencia");
+const resumoDesconto = document.querySelector("#resumo-desconto");
+const resumoTotal = document.querySelector("#resumo-total");
