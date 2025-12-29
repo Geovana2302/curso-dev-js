84f0436313949eae260f1110ecf9bb9a974a4cd1
Geovana Bicalho <geovanambicalho@hotmail.com>
2025-12-29 02:14:15 -0300

    Alteração no js para mostrar meus projetos

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 const projetos = [
-    { nome: "Cadastro", pasta: "cadastro", arquivo: "cadastro.html" },
-    { nome: "Orçamento", pasta: "orcamento", arquivo: "atividade_orcamento.html" }
+    { 
+        nome: "Cadastro", 
+        pasta: "cadastro", 
+        arquivo: "cadastro.html",
+        desc: "Projeto de cadastro desenvolvido durante o curso"
+    },
+    { 
+        nome: "Orçamento", 
+        pasta: "orcamento", 
+        arquivo: "atividade_orcamento.html",
+        desc: "Sistema simples para cálculo de orçamento"
+    }
 ];
 
 const container = document.getElementById('container-projetos');
 
 projetos.forEach(projeto => {
     const card = document.createElement('a');
-    // Ao usar apenas a barra no final, o navegador busca o index.html automaticamente
-    card.href = `./${projeto.pasta}/`; 
+
+    card.href = `./${projeto.pasta}/${projeto.arquivo}`;
     card.className = 'card';
-    
+    card.target = "_blank";
+
     card.innerHTML = `
         <h3>${projeto.nome}</h3>
         <p>${projeto.desc}</p>
         <span style="color: #f39c12; font-weight: bold;">Ver projeto →</span>
     `;
-    
+
     container.appendChild(card);
 });
